652a86a4f1a8536f996ece0f28fd2289b7c333e6
Sebastian madero <rockmadero@gmail.com>
2023-05-11 14:26:45 -0500

    assets

---
+++ assets/index.ts
@@ -0,0 +1,27 @@
+import aldea from './aldea.jpg';
+import bahia from './bahia-burbuejeante.jpg';
+import bastion from './bastion-brutal.jpeg';
+import cantera from './cantera-cinetica.jpg';
+import ciudadela from './cidudadela.png';
+import cruce from './cruce-kenjutsu.jpg';
+import ciudad from './cudad-mega.jpg';
+import granja from './granaja.jpg';
+import lab from './laboratorio.png';
+import muelle from './muelle.jpg';
+import plaza from './plaza.jpg';
+import termas from './termas.jpg';
+
+export {
+  aldea,
+  bahia,
+  bastion,
+  cantera,
+  ciudadela,
+  cruce,
+  ciudad,
+  granja,
+  lab,
+  muelle,
+  plaza,
+  termas
+};
