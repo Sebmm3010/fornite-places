b99adfa7131782bbf82535291fd9e80e7bdb2af0
Sebastian madero <rockmadero@gmail.com>
2023-05-11 19:35:30 -0500

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { data } from './data';
+import { useState } from 'react';
+import './app.css';
 function App() {
+  const { lugares } = data;
+  const [number, setNumber] = useState(0);
+  const randomPlace = () => {
+    const lata = Math.floor(Math.random() * lugares.length);
+    setNumber(lata);
+  };
   return (
     <>
-      <h1>Hola mundo</h1>
+      <main className="all">
+        <img src={lugares[number].img} alt="" className="img" />
+        <h1 className="title">{lugares[number].name}</h1>
+        {number === 12&&(<p>Haz econtrado al Quokka de la felicidad, puede elegir el lugar</p>)}
+        <button onClick={randomPlace}>Iniciar</button>
+      </main>
     </>
   );
 }
